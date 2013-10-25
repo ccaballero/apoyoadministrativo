49a3141b9fdc5d68506c7a740282ff7cfbe24a0a
Carlos E. Caballero B <cijkb.j@gmail.com>
2013-10-25 10:01:34 -0400

    [BASE] Working on base template and navigation

--- src/Cidetsi/BaseBundle/Controller/DefaultController.php
+++ src/Cidetsi/BaseBundle/Controller/DefaultController.php
@@ -9,4 +9,16 @@ class DefaultController extends Controller
     public function indexAction() {
         return $this->render('CidetsiBaseBundle:Default:index.html.twig');
     }
+    
+    public function menuAction() {
+        return $this->render('CidetsiBaseBundle:Default:menu.html.twig');
+    }
+    
+    public function toolbarAction() {
+        return $this->render('CidetsiBaseBundle:Default:toolbar.html.twig');
+    }
+    
+    public function footerAction() {
+        return $this->render('CidetsiBaseBundle:Default:footer.html.twig');
+    }
 }
