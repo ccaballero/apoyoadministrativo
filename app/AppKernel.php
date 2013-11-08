30303752a250371d9a52391fc0111c996c320413
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2013-11-07 22:24:36 -0400

    [PDF] the tcpdf integrated, and whiteoctober, so hard

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 
+            new WhiteOctober\TCPDFBundle\WhiteOctoberTCPDFBundle(),
+
             new Cidetsi\BaseBundle\CidetsiBaseBundle(),
             new Cidetsi\DepartamentoBundle\CidetsiDepartamentoBundle(),
         );
