a23bd0c6354aedd6547a7ec5e2e3832ef44701b2
Carlos E. Caballero B <cijkb.j@gmail.com>
2013-11-26 12:11:27 -0400

    [PDF] finished the seguimiento generator

--- src/Cidetsi/PdfReportBundle/Controller/DefaultController.php
+++ src/Cidetsi/PdfReportBundle/Controller/DefaultController.php
@@ -7,13 +7,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     public function testAction() {
+        return $this->render('CidetsiPdfReportBundle:Default:test.pdf.twig');
+
         $mpdfService = $this->get('tfox.mpdfport');
         $mpdfService->setAddDefaultConstructorArgs(false);
 
         $html = $this->renderView(
             'CidetsiPdfReportBundle:Default:test.pdf.twig');
         $arguments = array(
-            'constructorArgs' => array('UTF-8', 'Letter-L', '8', 'Helvetica', 20, 10, 10, 10, 30, 140, 'L'),
+            'constructorArgs' => array(
+                'UTF-8', 'Letter-L', '8', 'Helvetica',
+                20, 10, 10, 10, 30, 140, 'L'),
             'writeHtmlMode' => null,
             'writeHtmlInitialise' => null,
             'writeHtmlClose' => null,
@@ -25,13 +29,18 @@ class DefaultController extends Controller
     }
 
     public function seguimientoAction() {
+//        return $this->render('CidetsiPdfReportBundle:Default:seguimiento.pdf.twig');
+
         $mpdfService = $this->get('tfox.mpdfport');
         $mpdfService->setAddDefaultConstructorArgs(false);
 
         $html = $this->renderView(
             'CidetsiPdfReportBundle:Default:seguimiento.pdf.twig');
+
         $arguments = array(
-            'constructorArgs' => array('UTF-8', 'Letter-L', '8', 'Helvetica', 7, 7, 7, 7, 0, 0, 'L'),
+            'constructorArgs' => array(
+                'UTF-8', 'Letter-L', '8', 'Arial',
+                7, 7, 7, 7, 0, 0, 'L'),
             'writeHtmlMode' => null,
             'writeHtmlInitialise' => null,
             'writeHtmlClose' => null,
@@ -49,7 +58,9 @@ class DefaultController extends Controller
         $html = $this->renderView(
             'CidetsiPdfReportBundle:Default:nombramiento.pdf.twig');
         $arguments = array(
-            'constructorArgs' => array('UTF-8', 'Letter', '8', 'Helvetica', 22, 22, 22, 22, 0, 0, 'P'),
+            'constructorArgs' => array(
+                'UTF-8', 'Letter', '8', 'Helvetica',
+                22, 22, 22, 22, 0, 0, 'P'),
             'writeHtmlMode' => null,
             'writeHtmlInitialise' => null,
             'writeHtmlClose' => null,
